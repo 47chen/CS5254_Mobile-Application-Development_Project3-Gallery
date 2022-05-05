a35d584ce4d82b8bd32539fe8c0c6964af141870
47chen <shihchichen1010@vt.edu>
2022-05-04 21:06:29 -0400

    add progress bar when fetching api data

--- app/src/main/java/edu/vt/cs/cs5254/gallery/PhotoPageFragment.kt
+++ app/src/main/java/edu/vt/cs/cs5254/gallery/PhotoPageFragment.kt
@@ -9,6 +9,8 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import android.webkit.WebChromeClient
+import androidx.appcompat.app.AppCompatActivity
 
 private const val ARG_URI = "photo_page_url"
 
@@ -16,7 +18,7 @@ class PhotoPageFragment : Fragment() {
 
     private lateinit var uri: Uri
     private lateinit var webView: WebView
-
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         uri = arguments?.getParcelable(ARG_URI) ?: Uri.EMPTY
@@ -28,8 +30,24 @@ class PhotoPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
+        progressBar = view.findViewById(R.id.progress_bar)
+        progressBar.max = 100
+
         webView = view.findViewById(R.id.web_view)
         webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(webView: WebView, newProgress: Int) {
+                if (newProgress == 100) {
+                    progressBar.visibility = View.GONE
+                } else {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                (activity as AppCompatActivity).supportActionBar?.subtitle = title
+            }
+        }
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
         return view
